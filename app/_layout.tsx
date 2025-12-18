6e5b45a385911e9b56923b34eb6c7bd11c43a7cd
Arthur Papailhau <papailhau@gmail.com>
2025-12-17 18:00:37 -0800

    Add URL polyfill for Expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import 'react-native-url-polyfill/auto';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
